d880e73c930b084d9a295d1cbcdbecc215f2036b
Ricky Elrod <ricky@elrod.me>
2012-11-25 05:55:17 -0500

    Add (very simple, and possible errorprone) auto updating to the development version

--- src/main/scala/activity/MainActivity.scala
+++ src/main/scala/activity/MainActivity.scala
@@ -19,20 +19,75 @@
 package com.edgesysdesign.simpleradio
 
 import _root_.android.app.Activity
-import _root_.android.os.Bundle
+import _root_.android.content.{Context, Intent}
+import _root_.android.net.{ConnectivityManager, Uri}
+import _root_.android.os.{AsyncTask, Bundle}
+import _root_.android.text.{Editable, TextWatcher}
 import _root_.android.view.{Menu, MenuItem, View}
 import _root_.android.view.View.OnFocusChangeListener
-import _root_.android.content.Intent
 import _root_.android.widget.Toast
-import _root_.android.text.{TextWatcher, Editable}
+
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.methods.HttpGet
+
+// These are throwing an exception. It would be good to find a bug upstream
+// but I'm going to wait until Scala 2.10.0-RC3 comes out to see if it's fixed.
+// import scala.concurrent.future
+// import scala.concurrent.ExecutionContext.Implicits.global
+// Instead, we can rely on the old, deprecated version.
+import scala.concurrent.ops._
 
 class MainActivity extends Activity with TypedActivity {
+
+  implicit def toRunnable[F](f: => F): Runnable = new Runnable() { def run() = f }
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.receive)
     findView(TR.frequency).setText("145.170")
     findView(TR.pl_tone).setText("123.0 Hz")
     findView(TR.offset).setText("-600 KHz")
+
+    if (getString(R.string.development_build) == "true") {
+      // If we have a network connection, go ahead and check for an update.
+      val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE).asInstanceOf[ConnectivityManager]
+      val networkInfo = connMgr.getActiveNetworkInfo()
+
+      if (networkInfo != null && networkInfo.isConnected) {
+        spawn {
+          val client = new DefaultHttpClient()
+          val httpGet =
+            new HttpGet(s"${getString(R.string.updates_url)}/commit-sha.txt")
+
+          val result: Either[Throwable, String] = try {
+            val response = client.execute(httpGet)
+            val is = response.getEntity().getContent()
+            Right(scala.io.Source.fromInputStream(is).mkString.trim)
+          }
+          catch {
+            case e: Throwable => Left(e)
+          }
+
+          runOnUiThread {
+            result.fold(
+              left =>
+                Toast.makeText(
+                  this,
+                  getString(R.string.updates_error),
+                  Toast.LENGTH_SHORT).show(),
+              right => {
+                if (right != getString(R.string.version)) {
+                  val latestAPK =
+                    s"${getString(R.string.updates_url)}/simpleradio-$right.apk"
+                  val intent = new Intent(Intent.ACTION_VIEW, Uri.parse(latestAPK))
+                  startActivity(intent)
+                }
+              }
+            )
+          }
+        }
+      }
+    }
   }
 
   override def onCreateOptionsMenu(menu: Menu): Boolean = {
