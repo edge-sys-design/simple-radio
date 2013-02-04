52623621b0bce13d8d270383f7ce2883f409c9f8
Ricky Elrod <ricky@elrod.me>
2013-02-03 19:49:22 -0500

    Port two more programs from C to Scala.
    Test Plan: Ran locally.
    
    Reviewed By: jcarter

---
+++ util/AddPL.scala
@@ -0,0 +1,65 @@
+import java.io._
+import java.nio._
+
+object AddPL {
+  val sine = for (i <- 1 to 1024) yield 32768 * math.sin(i * (math.Pi / 1024)) toInt
+  val cosine = for (i <- 1 to 1024) yield 32768 * math.cos(i * (math.Pi / 1024)) toInt
+  val sampleRate = 44100
+
+  def main(args: Array[String]) {
+    require(
+      args.length == 2,
+      "Usage: scala AddPL inputfile outputfile")
+
+    val input = new File(args(0))
+    val output = new File(args(1))
+    require(input.exists, "The input file does not exist.")
+    require(output.canWrite, "The output file must be writable.")
+
+    val inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(input)))
+    val outputStream = new FileOutputStream(output).getChannel()
+    var position = 1
+    var sign = 1
+
+    val plTone = 131.8
+    val amplitude = 0.2
+
+    val move = (2 * sine.length * plTone) / sampleRate toInt
+
+    try {
+      Iterator
+        .continually(inputStream.readShort())
+        .foreach { sam =>
+          val sampleBuffer = ByteBuffer
+            .allocate(4)
+            .order(ByteOrder.BIG_ENDIAN)
+            .putShort(sam)
+            .order(ByteOrder.LITTLE_ENDIAN)
+
+          val sample = sampleBuffer.getShort(0)
+          val samOut = (((1 - amplitude) * sample.toFloat) + (amplitude * sign * sine(position))).toShort
+
+          val samples = ByteBuffer
+            .allocate(2)
+            .order(ByteOrder.LITTLE_ENDIAN)
+
+          samples
+            .asShortBuffer
+            .put(samOut)
+
+          position += move
+
+          if (position >= sine.size) {
+            position = position - sine.size
+            sign = -sign
+          }
+
+          outputStream.write(samples)
+        }
+    } catch {
+      case e: Throwable => {
+        outputStream.close()
+      }
+    }
+  }
+}
---
+++ util/FMDemod.scala
@@ -0,0 +1,59 @@
+import java.io._
+import java.nio._
+
+object FMDemod {
+  val bytesPerSample = 2
+
+  def main(args: Array[String]) {
+    require(
+      args.length == 2,
+      "Usage: scala FMDemod inputfile outputfile")
+
+    val input = new File(args(0))
+    val output = new File(args(1))
+    require(input.exists, "The input file does not exist.")
+    require(output.canWrite, "The output file must be writable.")
+
+    val inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(input)))
+    val outputStream = new FileOutputStream(output).getChannel()
+
+    case class Sample(var samI: Short, var samQ: Short)
+    val oldSample = Sample(0, 0)
+    val newSample = Sample(0, 0)
+
+    try {
+      Iterator
+        .continually(inputStream.readShort(), inputStream.readShort())
+        .foreach { sam =>
+          val sample = ByteBuffer
+            .allocate(4)
+            .order(ByteOrder.BIG_ENDIAN)
+            .putShort(sam._1)
+            .putShort(sam._2)
+            .order(ByteOrder.LITTLE_ENDIAN)
+
+          oldSample.samI = newSample.samI
+          oldSample.samQ = newSample.samQ
+
+          newSample.samI = sample.getShort(0)
+          newSample.samQ = sample.getShort(2)
+
+          val result = (((newSample.samI * oldSample.samQ) - (newSample.samQ * oldSample.samI)) / 65535.0).toShort
+
+          val samples = ByteBuffer
+            .allocate(2)
+            .order(ByteOrder.LITTLE_ENDIAN)
+
+          samples
+            .asShortBuffer
+            .put(result)
+
+          outputStream.write(samples)
+        }
+    } catch {
+      case e: Throwable => {
+        outputStream.close()
+      }
+    }
+  }
+}
